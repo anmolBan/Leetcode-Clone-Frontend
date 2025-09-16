f346f800dd01493a5e50205c8faeccc8ca5f0e04
Anmol Bansal <bansal.anmol98@gmail.com>
2025-09-16 18:12:03 +0530

    Update

---
+++ prisma/migrations/20250916121119_latest/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "Submission" ALTER COLUMN "status" DROP NOT NULL;
---
+++ prisma/migrations/20250916123717_latest/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Added the required column `status` to the `Submission` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Submission" DROP COLUMN "status",
+ADD COLUMN     "status" TEXT NOT NULL;
+
+-- DropEnum
+DROP TYPE "SubmissionStatus";
